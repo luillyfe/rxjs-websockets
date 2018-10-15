56011c9b8d900bbf68d7dee9e05b88b905270bbb
Fermin Blanco <luillyfe89@gmail.com>
2018-10-15 17:40:38 -0500

    Created lightweight news feed. Defined a listener for messages on Client. Format data to fit in html card. Defined a emitter to send messages to Server.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,46 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles.css";
 import { webSocket } from "rxjs/webSocket";
 
 const subject = webSocket("ws://localhost:3200");
 
-subject.subscribe(res => {
-    if (res.state === "ok") {
-        console.log(`Connection ${res.state}`);
+
+const setTopic = event => {
+    subject.next(event.target.innerText);
+};
+
+const setCardInfo = (
+    card = document,
+    options = {
+        urlToImage: "https://via.placeholder.com/350x150",
+        author: "Fermin Blanco",
+        content: "No content",
+        url: "#"
+    }) => {
+    const image = card.querySelector(".card-img-top");
+    const title = card.querySelector(".card-title");
+    const text = card.querySelector(".card-text");
+    const fullNews = card.querySelector(".full-news");
+
+    image.src = options.urlToImage;
+    title.innerText = options.author;
+    text.innerText = options.content;
+    fullNews.href = options.url;
+};
+
+subject.subscribe(response => {
+    if (response.state === "ok") {
+        console.log(`Connection ${response.state}`);
+
+        const topics = document.querySelector(".list-group.list-group-flush");
+        topics.addEventListener("click", setTopic);
     } else {
-        console.log(res)
+        console.log(response);
+
+        const carusellActive = [...Array.from(document.querySelector(".carousel-item.active").children)];
+        carusellActive.forEach((card, index) => {
+            const {urlToImage, author, content, url} = response[index];
+            setCardInfo(card, {urlToImage, author, content, url});
+        });
     }
 });
