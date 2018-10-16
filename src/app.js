c2166c0e4a428c7389db3d217e54ad5411c93dec
Fermin Blanco <luillyfe89@gmail.com>
2018-10-16 01:44:40 -0500

    Multiplexing capabilities added. Creating two Observables (Chat and News) to handle Chat and News UI components.

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
 import { webSocket } from "rxjs/webSocket";
+import { map } from "rxjs/operators";
 
-const subject = webSocket("ws://localhost:3200");
+const WebSocketSubject = webSocket("ws://localhost:3200");
 
 
-const setTopic = event => {
-    subject.next(event.target.innerText);
+const setTopic = (event = {target: {innerText: "Technology"}}) => {
+    WebSocketSubject.next(JSON.stringify(event.target.innerText));
 };
+const brokenImg = event => {
+    event.target.attr('src', 'https://via.placeholder.com/350x150');
+}
 
 const setCardInfo = (
     card = document,
@@ -28,19 +32,42 @@ const setCardInfo = (
     fullNews.href = options.url;
 };
 
-subject.subscribe(response => {
-    if (response.state === "ok") {
-        console.log(`Connection ${response.state}`);
+const chat = WebSocketSubject.multiplex(
+    () => JSON.stringify({subscribe: "chat"}),
+    () => JSON.stringify({unsubscribe: "chat"}),
+    message => message.type === "chat"
+).pipe(map(message => message.message));
 
-        const topics = document.querySelector(".list-group.list-group-flush");
-        topics.addEventListener("click", setTopic);
-    } else {
-        console.log(response);
+const news = WebSocketSubject.multiplex(
+    () => JSON.stringify({subscribe: "news"}),
+    () => JSON.stringify({unsubscribe: "news"}),
+    message => message.type === "news"
+).pipe(map(message => JSON.parse(message.articles)));
 
+WebSocketSubject.subscribe(
+    response => {
+        if (response.state === "ok") {
+            console.log(response.message);
+
+            const topics = document.querySelector(".list-group.list-group-flush");
+            const imgs = document.querySelector("img.card-img-top");
+
+            topics.addEventListener("click", setTopic);
+            imgs.addEventListener("onerror", brokenImg);
+        }
+    }, console.log, () => console.log("Websocket closed!"));
+
+news.subscribe(articles => {
+    if (articles.length >= 3) {
         const carusellActive = [...Array.from(document.querySelector(".carousel-item.active").children)];
         carusellActive.forEach((card, index) => {
-            const {urlToImage, author, content, url} = response[index];
+            const {urlToImage, author, content, url} = articles[index];
             setCardInfo(card, {urlToImage, author, content, url});
         });
+    } else {
+        console.log("It looks like there is not more content related to this topic. We switched the topic.");
+        setTopic();
     }
 });
+
+chat.subscribe(message => console.log(`Comming from Chat channel, ${message}`));
