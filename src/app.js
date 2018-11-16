f76ca22d3d8b21c537a07c912f3548053bd6ab75
Fermin Blanco <luillyfe89@gmail.com>
2018-11-16 00:18:20 -0500

    feat: rxjs/websocket integration with socket.io

--- src/app.js
+++ src/app.js
@@ -1,48 +1,32 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
 import { webSocket } from "rxjs/webSocket";
+import io from "socket.io-client";
+import { setCarouselInfo } from "./carousel";
 
-const subject = webSocket("ws://localhost:3200");
-
-
+const wsSubjectConfig = {
+    url: "ws://localhost:3200",
+    WebSocketCtor: io
+};
+const wsSubject = webSocket(wsSubjectConfig);
 const setTopic = event => {
-    subject.next(event.target.innerText);
+    wsSubject._socket.emit("topic", event.target.innerText);
 };
 
-const setCardInfo = (
-    card = document,
-    options = {
-        urlToImage: "https://via.placeholder.com/350x150",
-        author: "Fermin Blanco",
-        content: "No content",
-        url: "#"
-    }) => {
-    const image = card.querySelector(".card-img-top");
-    const title = card.querySelector(".card-title");
-    const text = card.querySelector(".card-text");
-    const fullNews = card.querySelector(".full-news");
-
-    image.src = options.urlToImage;
-    title.innerText = options.author;
-    text.innerText = options.content;
-    fullNews.href = options.url;
-};
-
-subject.subscribe(response => {
-    if (response.state === "ok") {
-        console.log(`Connection ${response.state}`);
-
-        const topics = document.querySelector(".list-group.list-group-flush");
-        topics.addEventListener("click", setTopic);
+wsSubject.subscribe();
+wsSubject._socket.on("articles", articles => {
+    if (articles && articles.length >= 3) {
+        setCarouselInfo(articles);
     } else {
-        if (response.length >= 3) {
-            const carusellActive = [...Array.from(document.querySelector(".carousel-item.active").children)];
-            carusellActive.forEach((card, index) => {
-                const {urlToImage, author, content, url} = response[index];
-                setCardInfo(card, {urlToImage, author, content, url});
-            });
-        } else {
-            console.log("We are having problems getting the news, please try again later!");
-        }
+        console.log("It looks like there is not more content related to this topic. We switched the topic.");
+        setTopic();
     }
 });
+
+
+
+/**
+ * init
+ * */
+const topics = document.querySelector(".list-group.list-group-flush");
+topics.addEventListener("click", setTopic);
